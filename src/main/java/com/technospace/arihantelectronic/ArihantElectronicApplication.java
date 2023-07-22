6764fc3cfa4df09045e017244d5afd8c58778c31
Janhavi-Khairnar <khairnarj2000@gmail.com>
2023-07-22 23:01:07 +0530

    [ADDED]:[Janhavi] Electronic Details App Setup

---
+++ src/main/java/com/technospace/arihantelectronic/ArihantElectronicApplication.java
@@ -0,0 +1,13 @@
+package com.technospace.arihantelectronic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArihantElectronicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArihantElectronicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technospace/arihantelectronic/ArihantElectronicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technospace.arihantelectronic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArihantElectronicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
